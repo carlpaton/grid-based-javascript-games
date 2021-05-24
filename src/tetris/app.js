b748fe29d9cb79f9d57c315085bb1db8f660c240
Carl Paton <carl.paton@xero.com>
2021-05-24 20:42:19 +1200

    foundations

---
+++ src/tetris/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.querySelector('#score');
+    const startButton = document.querySelector('#start-button');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
+})
