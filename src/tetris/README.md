60612d60c655fd2e562107fa10a5384eefeb2576
Carl Paton <carl.paton@xero.com>
2021-06-01 21:27:57 +1200

    all the updates, who reads this anyway? :D

--- src/tetris/README.md
+++ src/tetris/README.md
@@ -2,14 +2,18 @@
 
 I built this by following [Ania Kubów's tutorial](https://www.youtube.com/watch?v=w1JJfK09ujQ), thank you Ania for sharing your passion for code with the world.
 
+Her orginal code is here https://github.com/kubowania/Tetris-Basic, its encouraged not to copy but rather build and follow the tutorial.
+
 ## The grid
 
-Simple google sheets method to visualize the grid. Its 10 by 200 squares.
+Simple google sheets method to visualize the grid. Its 10 by 200 squares and zero based.
 
 ![Grid](grid-in-google-sheets.jpg)
 
 ## Tetrominoes
 
-These are the shapes that fall down and build up the lines, they can be rotated.
+- [View my Google sheets doc here](https://docs.google.com/spreadsheets/d/1AUCZV330Kh-ACKLboJx5S2HNRUaSG_O0gPRsVTWLxHM/edit?usp=sharing)
+
+These are the shapes that fall down and build up the lines, they can be rotated. The way I made sense of this in my head (based on the below) they rotated counter clockwise on B11 below.
 
 ![Tetrominoes](tetrominoes-possible-shapes.jpg)
--- src/tetris/js/app.js
+++ src/tetris/js/app.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded', () =>{
     const grid = document.querySelector('.grid');
     const scoreDisplay = document.querySelector('#score');
     const startButton = document.querySelector('#start-button');
-    let squares = Array.from(document.querySelectorAll('.grid div'));
     const width = 10;
+    
+    let squares = Array.from(document.querySelectorAll('.grid div'));
 
     // Tetrominoes
     const lTetromino = [
@@ -13,33 +14,61 @@ document.addEventListener('DOMContentLoaded', () =>{
         [width, width*2,width*2+1, width*2+2]
     ];
 
-    const theTetrominoes = [lTetromino];
+    const zTetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ];    
+
+    const theTetrominoes = [lTetromino, zTetromino];
 
     let currentPosition = 4; // starting point on the board
-    let current = theTetrominoes[0][0];
+    let currentRotation = 0;
+    let random = Math.floor(Math.random() * theTetrominoes.length);
+    //console.log('random = ' + random); 
+
+    let current = theTetrominoes[random][currentRotation];
 
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino');
-        })
+            //console.log('draw index = ' + index);
+        });
+    };
+
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino');
+            //console.log('undraw index = ' + index);            
+        });
+    };
+
+    timerId = setInterval(moveDown, 500);
+
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
     }
 
-    function getRandomInt(max) {
-console.log('max = ' + max);
+    function freeze() {
+        if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            
+            // let a new tetromino fall
+            random = Math.floor(Math.random() * theTetrominoes.length);
+            console.log('random = ' + random); 
+            console.log('currentRotation = ' + currentRotation); 
 
-let random = Math.random();
-console.log('Math.random() = ' + random);
+            current = theTetrominoes[random][currentRotation];
+            console.log('current = ' + current);
+            console.log('currentPosition = ' + currentPosition);
+            currentPosition = 4;
 
-let randomTimesMax = random * max;
-console.log('randomTimesMax = ' + randomTimesMax);
 
-let mathFloor = Math.floor(randomTimesMax);
-console.log('Math.floor(randomTimesMax) = ' + mathFloor);
-      }
-
-    let random = Math.floor(Math.random()*theTetrominoes.length);
-    // console.log(random); 
-    getRandomInt(3); 
-
-    draw();
+            draw();
+        }
+    }
 })
